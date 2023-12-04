e2b425db411ddcf51e1c9cef816e70c8840c03a0
kai10250920 <119949368+kai10250920@users.noreply.github.com>
2023-12-04 19:10:27 +0900

    main,js수정

--- YounSeokHo/main.js
+++ YounSeokHo/main.js
@@ -10,8 +10,6 @@ const app = express()
 const port = 8080
 
 
-const boardRouter = require('./board.js'); // board.js를 불러옴
-app.use('board', boardRouter); // /board 경로에 대한 라우터로 boardRouter를 사용
 // const writeRouter = require('./write'); // write.js를 불러옴
 
 // app.use('/write', writeRouter); // /write 경로에 대한 라우터로 writeRouter를 사용
@@ -55,23 +53,25 @@ app.get('/main', (req, res) => {
   `<hr>
   <h2>메인 페이지에 오신 것을 환영합니다</h2>
   <p>로그인에 성공하셨습니다.</p>
-  <a href="/write-introduction">자기소개서 작성</a>
+  <a href="/write-resume
+">자기소개서 작성</a>
   <br>
-  <a href="/view-introduction">내 자기소개서 보기</a> | <a href="/edit-introduction">자기소개서 수정</a>
+  <a href="/view-resume">내 자기소개서 보기</a> | <a href="/edit-resume">자기소개서 수정</a>
   <br>
   <a href="/write-notice">공고글 작성</a>
   <a href="/view-mynotices">내 작성글 보기</a>
   <a href="/view-allnotice">모든 공고글 보기</a>
   <a href="/view-messages">내가 받은 메시지 확인</a>
+  <a href="/sent-requests">내가 보낸 메시지 확인</a>
 
-  `, // 버튼 링크 추가
+  `, // 버튼 링크  추가
   authCheck.statusUI(req, res)
 );
 res.send(html);
 });
 
 
-app.get('/write-introduction', (req, res) => {
+app.get('/write-resume', (req, res) => {
   if (!authCheck.isOwner(req, res)) {
     res.redirect('/auth/login');
     return false;
@@ -81,15 +81,16 @@ app.get('/write-introduction', (req, res) => {
   console.log("userid",userId)
 
   // my 테이블에서 자기소개서 정보 불러오기
-  db.query('SELECT * FROM my WHERE id = ?', [userId], function (error, results, fields) {
+  db.query('SELECT * FROM my WHERE  id = ?', [userId], function (error, results, fields) {
     if (error) throw error;
 
     // 자기소개서 정보가 없는 경우 빈 값을 기본값으로 설정
     // 자기소개서 작성 페이지 렌더링
-    var html = template.HTML('Write Introduction',
+    var html = template.HTML('Write resume',
       `<hr>
         <h2>자기소개서 작성</h2>
-        <form action="/write-introduction-process" method="post">
+        <form action="/write-resume
+      " method="post">
           <p><input type="text" name="name"  placeholder="이름"></p>
           <p><input type="text" name="callnum"  placeholder="전화번호"></p>
           <p><input type="submit" value="작성 완료"></p>
@@ -102,7 +103,7 @@ app.get('/write-introduction', (req, res) => {
 });
 
   
-app.post('/write-introduction-process', (req, res) => {
+app.post('/write-resume', (req, res) => {
   if (!authCheck.isOwner(req, res)) {
     res.redirect('/auth/login');
     return false;
@@ -117,11 +118,12 @@ app.post('/write-introduction-process', (req, res) => {
   var name = req.body.name;
   var callnum = req.body.callnum;
   if (!name || !callnum) {
-    // Send an alert message and redirect back to the write introduction page
+    // Send an alert message and redirect back to the write resume page
     res.send(
       `<script>
         alert("이름과 전화번호를 모두 입력하세요.");
-        window.location.href = '/write-introduction';
+        window.location.href = '/write-resume
+      ';
       </script>`
     );
     return false;
@@ -134,13 +136,14 @@ app.post('/write-introduction-process', (req, res) => {
     if (results.length > 0) {
       // 이미 자기소개서 정보가 있는 경우, 업데이트
       db.query('UPDATE my SET name = ?, callnum = ? WHERE id = ?', [name, callnum, userId], function (error, data) {
+        
         if (error) throw error;
         res.redirect('/main');  // 업데이트 후 메인 페이지로 리다이렉션
       });
     } 
   });
 });
-app.get('/view-introduction', (req, res) => {
+app.get('/view-resume', (req, res) => {
   if (!authCheck.isOwner(req, res)) {
         res.redirect('/auth/login');
 
@@ -154,14 +157,14 @@ app.get('/view-introduction', (req, res) => {
     if (error) throw error;
 
     // 자기소개서 정보가 없는 경우 빈 값을 기본값으로 설정
-    var introduction = results.length > 0 ? results[0] : { name: '', callnum: '' };
+    var resume = results.length > 0 ? results[0] : { name: '', callnum: '' };
 
     // 자기소개서 보기 페이지 렌더링
-    var html = template.HTML('View Introduction',
+    var html = template.HTML('View resume',
       `<hr>
         <h2>내 자기소개서 보기</h2>
-        <p><strong>이름:</strong> ${introduction.name}</p>
-        <p><strong>전화번호:</strong> ${introduction.callnum}</p>
+        <p><strong>이름:</strong> ${resume.name}</p>
+        <p><strong>전화번호:</strong> ${resume.callnum}</p>
         <a href="/main">메뉴돌아가기</a>`, // 자기소개서 수정 버튼 추가
       authCheck.statusUI(req, res)
     );
@@ -169,7 +172,7 @@ app.get('/view-introduction', (req, res) => {
   });
 });
 
-app.get('/edit-introduction', (req, res) => {
+app.get('/edit-resume', (req, res) => {
   if (!authCheck.isOwner(req, res)) {
     res.redirect('/auth/login');
     return false;
@@ -182,15 +185,16 @@ app.get('/edit-introduction', (req, res) => {
     if (error) throw error;
 
     // 자기소개서 정보가 없는 경우 빈 값을 기본값으로 설정
-    var introduction = results.length > 0 ? results[0] : { name: '', callnum: '' };
+    var resume = results.length > 0 ? results[0] : { name: '', callnum: '' };
 
     // 자기소개서 수정 페이지 렌더링
-    var html = template.HTML('Edit Introduction',
+    var html = template.HTML('Edit resume',
       `<hr>
         <h2>자기소개서 수정</h2>
-        <form action="/edit-introduction-process" method="post">
-          <p><input type="text" name="name" value="${introduction.name}" placeholder="이름"></p>
-          <p><input type="text" name="callnum" value="${introduction.callnum}" placeholder="전화번호"></p>
+        <form action="/edit-resume
+      " method="post">
+          <p><input type="text" name="name" value="${resume.name}" placeholder="이름"></p>
+          <p><input type="text" name="callnum" value="${resume.callnum}" placeholder="전화번호"></p>
           <p><input type="submit" value="수정 완료"></p>
         </form>`,
       authCheck.statusUI(req, res)
@@ -200,7 +204,7 @@ app.get('/edit-introduction', (req, res) => {
   
 });
 
-app.post('/edit-introduction-process', (req, res) => {
+app.post('/edit-resume', (req, res) => {
   if (!authCheck.isOwner(req, res)) {
     res.redirect('/auth/login');
     return false;
@@ -257,7 +261,7 @@ app.get('/write-notice', (req, res) => {
     </head>
     <body>
         <h1>글 작성</h1>
-        <form action="/board/write" method="post">
+        <form action="/write-notice" method="post">
             <table border="1">
                 <tr>
                     <td>작성자</td>
@@ -286,7 +290,7 @@ app.get('/write-notice', (req, res) => {
   res.send(html);
 });
 
-app.post('/board/write', (req, res) => {
+app.post('/write-notice', (req, res) => {
   if (!authCheck.isOwner(req, res)) {
     res.redirect('/auth/login');
     return false;
@@ -295,7 +299,7 @@ app.post('/board/write', (req, res) => {
 
   const title = req.body.title;
   const content = req.body.content;
-  // writing 테이블에 데이터 추가, wid는 자동으로 증가
+  
   db.query('INSERT INTO writing (writeremail, title, content) VALUES (?, ?, ?)', [writeemail, title, content], function (error, data) {
     if (error) {
       console.error(error);
@@ -400,7 +404,7 @@ app.get('/view-mynotices/:wid', (req, res) => {
     res.send(html);
   });
 });
-
+3
 
 
 app.get('/view-allnotice', (req, res) => {
@@ -422,7 +426,7 @@ app.get('/view-allnotice', (req, res) => {
       `<hr>
       <h2>모든 공고글</h2>
       <ul>
-        ${formattedPosts.map(post => `<li><a href="/view-allnotice/${post.wid}">${post.title}</a> - 작성일자: ${post.created_at}</li>`).join('')}
+        ${formattedPosts.map(post => `<li><a href="/view-allnotice/${post.wid}">${post.title}</a>번호- ${post.wid} - 작성일자: ${post.created_at} </li>`).join('')}
       </ul>
       <a href="/main">메뉴로 돌아가기</a>`,
       authCheck.statusUI(req, res)
@@ -441,7 +445,7 @@ app.get('/view-allnotice/:wid', (req, res) => {
   const wid = req.params.wid;
 
   // writing 테이블에서 해당 글의 내용을 가져옴
-  db.query('SELECT * FROM writing WHERE writeremail = ? AND wid = ?', [writeemail, wid], function (error, results, fields) {
+  db.query('SELECT * FROM writing WHERE   wid = ?', [ wid], function (error, results, fields) {
     if (error) {
       console.error(error);
       // 에러 메시지를 클라이언트에게 전송하거나 처리할 수 있는 로직 추가
@@ -465,7 +469,7 @@ app.get('/view-allnotice/:wid', (req, res) => {
       <p style="font-weight: bold;">작성자: ${results[0].writeremail}</p>
       <p style="white-space: pre-line;">${results[0].content}</p>
       <p style="font-size: 12px;">작성일자: ${formattedDate}</p>
-      <button onclick="sendRequest('${results[0].writeremail}')">요청 보내기</button>
+      <a href="/send-message?writeremail=${results[0].writeremail}&content=요청 메시지 내용">메시지 전송하기</a>
       </div>
     <a href="/view-allnotice">모든 공고글 보기로 돌아가기</a>`,
     authCheck.statusUI(req, res)
@@ -475,35 +479,244 @@ app.get('/view-allnotice/:wid', (req, res) => {
   });
 });
 
-app.post('/send-message', authCheck, (req, res) => {
+
+
+app.get('/send-message', (req, res) => {
   if (!authCheck.isOwner(req, res)) {
     res.redirect('/auth/login');
-    return;
+    return false;
   }
 
-  const { recipient, message } = req.body;
-  const sender = req.session.email;
+  const senderemail = req.session.mail; // 현재 로그인한 사용자의 이메일
+  const getteremail = req.query.writeremail; // URL 쿼리 매개변수로 전달된 받는 사람의 이메일
 
-  if (!recipient || !message) {
-    res.status(400).send('수신자와 메시지를 모두 입력하세요.');
-    return;
-  }
-
-  db.run(
-    'INSERT INTO messages (sender_email, recipient_email, message) VALUES (?, ?, ?)',
-    [sender, recipient, message],
-    (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('메시지 전송 중 오류가 발생했습니다.');
-      } else {
-        res.redirect('/main');
-      }
-    }
+  const html = template.HTML('Send Message',
+    `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>메시지 전송</title>
+        <link rel='stylesheet' href='/stylesheets/style.css'/>
+    </head>
+    <body>
+        <h1>메시지 전송</h1>
+        <form action="/send-message" method="post">
+            <input type="hidden" name="senderemail" value="${senderemail}">
+            <input type="hidden" name="receiveremail" value="${getteremail}">
+            <p>받는 사람: ${getteremail}</p>
+            <textarea name="content" cols="30" rows="10" required></textarea>
+            <br>
+            <button type="submit">메시지 전송</button>
+        </form>
+    </body>
+    </html>
+    `,
+    authCheck.statusUI(req, res)
   );
+
+  res.send(html);
+});
+
+// 메시지 전송 처리 라우터 설정
+app.post('/send-message', (req, res) => {
+  const { senderemail, receiveremail, content } = req.body;
+
+  // 여기서 데이터베이스에 메시지를 저장
+  db.query('INSERT INTO request (senderemail, getteremail, content) VALUES (?, ?, ?)', [senderemail, receiveremail, content], function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      // 에러 처리 로직 추가
+      return res.status(500).json({ success: false, message: '메시지 저장 중 오류가 발생했습니다.' });
+    }
+
+    // 클라이언트에 응답을 보냅니다.
+    res.json({ success: true, message: '메시지가 성공적으로 전송되었습니다.', redirectTo: '/main' });
+  });
+});
+
+app.get('/view-messages', (req, res) => {
+  if (!authCheck.isOwner(req, res)) {
+    res.redirect('/auth/login');
+    return false;
+  }
+
+  const getteremail = req.session.mail;
+
+  // getteremail이 현재 사용자의 이메일과 같은 항목들을 가져옴
+  db.query('SELECT * FROM request WHERE getteremail = ?', [getteremail], function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      // 에러 메시지를 클라이언트에게 전송하거나 처리할 수 있는 로직 추가
+      return res.status(500).send('받은 메시지를 불러오는 중 에러가 발생했습니다.');
+    }
+
+    // 가져온 메시지 목록을 표 형태로 나타내고, 각각의 항목을 클릭하면 해당 내용을 확인할 수 있는 HTML 생성
+    const html = template.HTML('받은 메시지 목록',
+      `<hr>
+      <h2>받은 메시지 목록</h2>
+      <ul>
+        ${results.map(message => `<li><a href="/view-message/${message.requestid}">${message.senderemail}에서 온 메시지</a></li>`).join('')}
+      </ul>
+      <a href="/main">메뉴로 돌아가기</a>`,
+      authCheck.statusUI(req, res)
+    );
+    res.send(html);
+  });
+});
+
+app.get('/view-message/:request_id', (req, res) => {
+  if (!authCheck.isOwner(req, res)) {
+    res.redirect('/auth/login');
+    return false;
+  }
+
+  const request_id = req.params.request_id;
+
+  // request_id에 해당하는 메시지 내용을 가져옴
+  db.query('SELECT * FROM request WHERE requestid = ?', [request_id], function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      // 에러 메시지를 클라이언트에게 전송하거나 처리할 수 있는 로직 추가
+      return res.status(500).send('메시지를 불러오는 중 에러가 발생했습니다.');
+    }
+
+    if (results.length === 0) {
+      // 해당하는 메시지가 없을 경우 예외 처리
+      return res.status(404).send('해당하는 메시지를 찾을 수 없습니다.');
+    }
+
+    // 가져온 메시지 내용을 HTML 형식으로 표시
+    const html = template.HTML('메시지 내용 확인',
+    `<hr>
+    <h2>메시지 내용 확인</h2>
+    <div>
+      <p><strong>보낸 사람:</strong> ${results[0].senderemail}</p>
+      <p><strong>내용:</strong> ${results[0].content}</p>
+      ${results[0].answer === null ?
+        `<a href="/respond-to-request/${request_id}/accept">수락</a>
+         <a href="/respond-to-request/${request_id}/reject">거절</a>` :
+        `<p>이미 응답하였습니다.</p>`
+      }
+    </div>
+    <a href="/view-messages">받은 메시지 목록으로 돌아가기</a>`,
+    authCheck.statusUI(req, res)
+  );
+//         <button onclick="respondToRequest(${request_id}, true)">수락</button>
+{/* <button onclick="respondToRequest(${request_id}, false)">거절</button> */}
+    res.send(html);
+  });
+});
+
+// 요청에 대한 응답 처리
+app.get('/respond-to-request/:request_id/:action', (req, res) => {
+  if (!authCheck.isOwner(req, res)) {
+    res.redirect('/auth/login');
+    return false;
+  }
+
+  const request_id = req.params.request_id;
+  const action = req.params.action;
+
+  // request_id에 해당하는 요청 정보를 가져옴
+  db.query('SELECT * FROM request WHERE requestid = ?', [request_id], function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      // 에러 처리 로직 추가
+      return res.status(500).json({ success: false, message: '요청 정보를 불러오는 중 오류가 발생했습니다.' });
+    }
+
+    if (results.length === 0) {
+      // 해당하는 요청이 없을 경우 예외 처리
+      return res.status(404).json({ success: false, message: '해당하는 요청을 찾을 수 없습니다.' });
+    }
+
+    if (results[0].answer !== null) {
+      // 이미 응답한 경우
+      return res.status(400).json({ success: false, message: '이미 응답하였습니다.' });
+    }
+
+    // action에 따라 수락 또는 거절 처리
+    const answerValue = action === 'accept' ? 1 : 2;
+
+    // request_id에 해당하는 요청에 대한 응답을 업데이트
+    db.query('UPDATE request SET answer = ? WHERE requestid = ?', [answerValue, request_id], function (error, updateResults, fields) {
+      if (error) {
+        console.error(error);
+        // 에러 처리 로직 추가
+        return res.status(500).json({ success: false, message: '응답 처리 중 오류가 발생했습니다.' });
+      }
+
+      // 응답이 성공적으로 처리되면 받은 메시지 목록으로 리다이렉트
+      res.redirect('/view-messages');
+    });
+  });
 });
 
 
+
+app.get('/sent-requests', (req, res) => {
+  if (!authCheck.isOwner(req, res)) {
+    res.redirect('/auth/login');
+    return false;
+  }
+
+  const senderemail = req.session.mail;
+
+  // senderemail에 해당하는 요청 목록을 가져옴
+  db.query('SELECT * FROM request WHERE senderemail = ?', [senderemail], function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      // 에러 처리 로직 추가
+      return res.status(500).send('요청 목록을 불러오는 중 에러가 발생했습니다.');
+    }
+
+    // 가져온 요청 목록을 HTML 형식으로 표시
+    const html = template.HTML('보낸 요청 확인',
+      `<hr>
+      <h2>보낸 요청 목록</h2>
+      <ul>
+        ${results.map(request => `
+          <li>
+            <p><strong>받는 사람:</strong> ${request.getteremail}</p>
+            <p><strong>내용:</strong> ${request.content}</p>
+            ${request.answer === null ? '응답 대기 중' : request.answer === 1 ? '수락됨' : '거절됨'}
+          </li>
+        `).join('')}
+      </ul>
+      <a href="/">메인 메뉴로 돌아가기</a>`,
+      authCheck.statusUI(req, res)
+    );
+
+    res.send(html);
+  });
+});
+
+
+app.get('/get-sent-request-status/:request_id', (req, res) => {
+  if (!authCheck.isOwner(req, res)) {
+    res.redirect('/auth/login');
+    return false;
+  }
+
+  const request_id = req.params.request_id;
+
+  // request_id에 해당하는 요청의 응답 상태를 가져옴
+  db.query('SELECT answer FROM request WHERE requestid = ?', [request_id], function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      // 에러 처리 로직 추가
+      return res.status(500).json({ success: false, message: '응답 상태를 불러오는 중 오류가 발생했습니다.' });
+    }
+
+    if (results.length === 0) {
+      // 해당하는 요청이 없을 경우 예외 처리
+      return res.status(404).json({ success: false, message: '해당하는 요청을 찾을 수 없습니다.' });
+    }
+
+    // 응답 상태를 클라이언트에 전송
+    res.json({ success: true, answer: results[0].answer });
+  });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
